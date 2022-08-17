4f13dd007d0b36f16ced8291a220dc9d1af20272
Archibald THIRION <athirion@e1r4p22.clusters.42paris.fr>
2022-08-17 12:05:15 +0200

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athirion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 09:56:34 by athirion          #+#    #+#             */
+/*   Updated: 2022/08/17 10:26:52 by athirion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+int main(int ac, char **av)
+{
+	int	len;
+
+	(void) av;
+	len = 0;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < ac; i ++)
+		{
+			len = std::strlen(av[i]);
+			for (int j = 0; j < len; j ++)
+				av[i][j] = std::toupper(av[i][j]);
+			std::cout << av[i];
+		}
+		std::cout << "\n";
+	}
+	return (0);
+}
