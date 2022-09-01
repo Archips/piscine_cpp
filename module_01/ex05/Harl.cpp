d26cbff715f9075ee6dfed65b42068aacf6b48aa
Archibald Thirion <archibald@archibalds-macbook-pro.home>
2022-09-01 19:20:30 +0200

    m01 ex05 in progress

---
+++ module_01/ex05/Harl.cpp
@@ -0,0 +1,29 @@
+#include "Harl.hpp"
+
+Harl::Harl(void) {
+
+}
+
+Harl::~Harl(void) {
+
+}
+
+void    Harl::debug() const {
+
+}
+
+void    Harl::info() const {
+
+}
+
+void    Harl::error() const {
+
+}
+
+void    Harl::warning() const {
+
+}
+
+void    Harl::complain(std::string level) {
+    (void) level;
+}
