6529ed3ddca640ec8fc235a26bf58e265a7e0670
Archibald THIRION <athirion@e3r4p23.clusters.42paris.fr>
2022-09-07 14:17:29 +0200

    m02 ex03 Point.hpp in progress

---
+++ module_02/ex03/Point.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athirion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 14:08:38 by athirion          #+#    #+#             */
+/*   Updated: 2022/09/07 14:16:53 by athirion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINT_HPP
+# defnie POINT_HPP
+
+class Point {
+
+	public:
+
+	Point(void);
+	Point(Point const &src);
+	~Point(void);
+
+	Point & operator=(Point const &rhs);
+
+
+
+	private:
+
+
+
+
+};
+
+#endif
