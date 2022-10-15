0033d5ee44d4cbe19aa4a019bdc468baf8fc5a23
Archibald Thirion <archibald@archibalds-macbook-pro.home>
2022-10-15 09:27:44 +0200

    m05 init

---
+++ module_05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,14 @@
+#ifndef BUREAUCRAT_HPP
+
+class Bureaucrat {
+
+    public:
+
+        Bureaucrat(void);
+        Bureaucrat()
+
+    private:
+
+        const std::string   _name;
+        int                 _grade;
+};
