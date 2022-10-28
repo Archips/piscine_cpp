aa067a50595776f15bb1e58f03404cd5d9de2f98
Archibald THIRION <athirion@e3r9p3.clusters.42paris.fr>
2022-10-28 15:49:47 +0200

    m07 ex02 cleaned

--- module_07/ex02/src/main.cpp
+++ module_07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athirion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 17:06:43 by athirion          #+#    #+#             */
-/*   Updated: 2022/10/28 11:12:27 by athirion         ###   ########.fr       */
+/*   Updated: 2022/10/28 15:49:14 by athirion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -155,61 +155,6 @@ int main(void)
 		cpy2.display();
 		std::cout << std::endl;
 	}
-
-/* 	Array<std::string> array(10); */
-/* 		Array<std::string> cpy; */
-
-/* 		for (int i = 0; i < 10; i ++) */
-/* 		{ */
-/* 			if (i % 2 == 0) */
-/* 				array[i] = "salut"; */
-/* 			else */
-/* 				array[i] = "bye"; */
-/* 		} */
-
-/* 		array.display(); */
-
-/* 		try { */
-
-/* 			std::cout << array[10] << std::endl; */
-/* 		} */
-/* 		catch (const std::exception &e) */
-/* 		{ */
-/* 			std::cout << "Error: " << e.what() << std::endl; */
-/* 		} */
-
-/* 		cpy = array; */
-
-/* 		cpy.display(); */
-/* 		cpy.size(); */
-/* 		try { */
-/* 			for (int i = 0; i < 11; i ++) */
-/* 				cpy[i] = "NOPE"; */
-/* 		} */
-/* 		catch (const std::exception &e) { */
-/* 			std::cout << "Error: " << e.what() << std::endl;} */
-/* 		std::cout << "COPY :" << std::endl; */
-/* 		cpy.display(); */
-/* 		std::cout << "ARRAY :" << std::endl; */
-/* 		array.display(); */
-
-/*         Array<std::string> cpy2(cpy); */
-/*         std::cout << "COPY 2:" << std::endl; */
-/*         cpy2.display(); */
-
-/*         try { */
-/*             for (int i = 0; i < 10; i ++) */
-/*                 cpy2[i] = "NOPEEEEEEEEEE"; */
-/*         } */
-/*         catch (const std::exception &e) { */
-/*             std::cout << "Error: " << e.what() << std::endl;} */
-
-/*         std::cout << "COPY :" << std::endl; */
-/*         cpy.display(); */
-/*         std::cout << "COPY 2:" << std::endl; */
-/*         cpy2.display(); */
-
-/* 	} */
 
 	return (0);
 }
