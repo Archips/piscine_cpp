f69f60b6573dcecf0ac2e3153600aa7f5d517175
Archibald Thirion <archibald@archibalds-macbook-pro.home>
2022-10-30 17:55:15 +0100

    m08 ex02 done - implement main

--- module_08/ex02/include/MutantStack.hpp
+++ module_08/ex02/include/MutantStack.hpp
@@ -1,4 +1,32 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-class MutantStack
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+
+public:
+
+    MutantStack(void): std::stack<T>(){}
+    MutantStack(const MutantStack<T> &src): std::stack<T>(src) {*this = src;}
+    ~MutantStack(void){}
+
+    MutantStack<T>  operator=(const MutantStack<T> &rhs) {
+        if (this != &rhs)
+            this->c = rhs.c;
+        return (*this);
+    }
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    iterator begin(void) {
+        return (this->c.begin());
+    }
+    iterator end(void) {
+        return (this->c.end());
+    }
+
+};
+
+#endif
